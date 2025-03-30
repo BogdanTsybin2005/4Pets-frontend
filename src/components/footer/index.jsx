5b8eac0589f4844aea6301485e3e67ae94db372b
BogdanTsybin2005 <tsybinb@gmail.com>
2025-03-30 15:40:18 +0600

    Сделал футер для веб сайта чтобы его можно было применять на разных страницах

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,4 +1,6 @@
 import Main4PetsLogo from "../../svg_pictures/4pets-logo"
+import InstagramIcon from '../../svg_pictures/instagram-icon';
+import './style.css';
 
 
 
@@ -6,7 +8,28 @@ export default function Footer() {
     return (
         <>
             <div className="footer">
-                <Main4PetsLogo width={306} height={162}/>
+                <div className="footer-container">
+                    <div className="footer__4pets-icon-block">
+                        <Main4PetsLogo width={306} height={162}/>
+                    </div>
+                    <div className="footer-contacts-information-block">
+                        <div className="footer__instagram-info">
+                            <div className="footer__instagram-icon-block">
+                                <InstagramIcon/>
+                            </div>
+                            <div className="footer__instagram-links-block">
+                                <a href="https://www.instagram.com/4petssss/" target="blank">@4petssss</a>
+                                <a href="https://www.instagram.com/tsi.auca.kg/" target="blank">@tsiauca.kg</a>
+                            </div>
+                        </div>
+                        <div className="footer__contacts-info">
+                            <p>+996 502 296 444, </p>
+                            <p>+996 705 916 001</p>
+                            <p>Email:</p>
+                            <p>4pets.tsi@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -6,7 +6,7 @@ import allMyLanguageData from "../../src/data/data";
 const LanguageContext = createContext();
 
 export function LanguageProvider({ children }) {
-  const [interfaceLanguage, setInterfaceLanguage] = useState("en");
+  const [interfaceLanguage, setInterfaceLanguage] = useState("kg");
 
   return (
     <LanguageContext.Provider value={{ interfaceLanguage, setInterfaceLanguage, allMyLanguageData }}>
